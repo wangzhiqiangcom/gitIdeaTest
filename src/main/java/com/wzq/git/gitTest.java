afa4c3bfe26a9c4a81724498e008acb3802094b8
wangzhiqiang <wangzhiqiangcom@163.com>
2021-08-17 15:19:12 +0800

    second commit

--- src/main/java/com/wzq/git/gitTest.java
+++ src/main/java/com/wzq/git/gitTest.java
@@ -3,5 +3,6 @@ package com.wzq.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello idea.git");
+        System.out.println("hell idea.git2");
     }
 }
